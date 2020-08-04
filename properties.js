dd134d947dfbec1722b5612852b9f13cd756854d
chiragj22 <69108719+chiragj22@users.noreply.github.com>
2020-08-04 12:20:13 +0530

    Add files via upload

--- properties.js
+++ properties.js
@@ -18,5 +18,19 @@ function toggleDuration () {
     if (this.className === 'stay-duration passive'){
         document.getElementsByClassName('active')[0].className = 'stay-duration passive';
         this.className = 'stay-duration active';
+
+        if (this.innerHTML === 'Short Stay <span>(Upto 14 Days)</span>') {
+            var price = document.getElementsByClassName('property-pricing-container');
+            for (var i = 0; i < price.length ; i++) {
+                var cost = price[i].getElementsByTagName('span')[0].className;
+                price[i].getElementsByTagName('span')[0].innerHTML = cost;
+            }
+        } else {
+            var price = document.getElementsByClassName('property-pricing-container');
+            for (var i = 0; i < price.length ; i++) {
+                var cost = price[i].getElementsByTagName('span')[0].id;
+                price[i].getElementsByTagName('span')[0].innerHTML = cost;
+            }
+        }
     }
 }
